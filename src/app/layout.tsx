de9d9ab7d87ea1d3cd65fd5b0270617dd6016715
ShivamV952 <sv2923976@gmail.com>
2026-01-11 02:38:09 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 import "./globals.css";
-import "antd/dist/reset.css";
 const inter = Lexend({ weight: ["400"], subsets: ["latin"] });
 
 
